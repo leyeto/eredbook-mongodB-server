00c0fdf53ecd3d338bb921860bf45238d90f97fd
Adeleye Ifaturoti <adeleyeifaturoti@yahoo.com>
2023-02-04 16:37:10 +0000

    added clinician basic types

---
+++ basicModels/clinician.ts
@@ -0,0 +1,10 @@
+export type Clinician = {
+  firstName: String;
+  lastName: String;
+  username: String;
+  password: String;
+  role: String;
+  badgeNumber: String;
+  NMCPin?: String;
+  Department: String;
+};
