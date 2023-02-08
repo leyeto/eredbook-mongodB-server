8a0591e697ca4ce1d670cb2183d27b895d4e838c
Adeleye Ifaturoti <adeleyeifaturoti@yahoo.com>
2023-02-08 05:25:50 +0000

    ChildSchema for Mongoose type done

--- mongooseModels/Child.ts
+++ mongooseModels/Child.ts
@@ -5,9 +5,18 @@ const { Schema } = mongoose;
 const ChildSchema = new Schema({
   firstName: { type: String },
   lastName: { type: String },
-  nhsNumber: { type: String },
-  birthWeight: { type: Number },
+  dateOfBirth: { type: Date },
+  address: { type: String },
+  birthWeightInKg: { type: Number },
   birthHeight: { type: Number },
+  nhsNumber: { type: String },
+  birthHospital: { type: String },
+  picture: { type: String },
+  bloodGroup: {
+    type: String,
+    enum: ["A+", "A-", "B+", "B-", "O+", "O-", "AB+", "AB-"],
+  },
+  genotype: { type: String, enum: ["AA", "AS", "SS", "AC"] },
 });
 
 export default ChildSchema;
